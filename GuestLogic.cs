550bdaeaa7e7316262e555bd7374a89e7d789b80
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-10 17:31:22 +0100

    updated main code

---
+++ GuestLogic.cs
@@ -0,0 +1,38 @@
+﻿
+namespace GuestBookDemo
+{
+	public static class GuestLogic
+	{
+		public static void WelcomeMessage()
+		{
+            Console.WriteLine("Welcome to the Guest Book App");
+            Console.WriteLine("******************************");
+            Console.WriteLine();
+        }
+
+        public static string GetPartyName()
+        {
+            Console.Write("What is your party/group name?: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        public static int GetPartySize()
+        {
+            bool isValidNumber;
+            int output;
+
+            do
+            {
+
+                Console.Write("How many people are in your party?: ");
+                string partySizeText = Console.ReadLine();
+                isValidNumber = int.TryParse(partySizeText, out output);
+            } while (isValidNumber == false);
+
+            return output;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿
 
 //1.Welcome the user(Intro the app name as well)
-    //Ask user for their name
-    //Store the name
+//Ask user for their name
+//Store the name
 
 
 //2. Ask user for number in party
-    //<convert string to number>
-    //Add the number to the total
+//<convert string to number>
+//Add the number to the total
 
 
 //3. Check to see if more guests are coming
-    //Loop back to the top until no more guests
+//Loop back to the top until no more guests
 
 
 //4. Loop over the names list and print them
@@ -19,8 +19,13 @@
 
 //5. Print the total guest count
 
+using GuestBookDemo;
 
+// WelcomeMessage
+GuestLogic.WelcomeMessage();
 
+// GetPartyName
+string partyName = GuestLogic.GetPartyName();
 
-
-
+// GetPartySize
+int partySize = GuestLogic.GetPartySize();
