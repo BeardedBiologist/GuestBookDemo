6d6f2e7b68d92b082e72a2002367b41ef7d22a69
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-10 18:07:52 +0100

    classes added, code condensed

--- GuestLogic.cs
+++ GuestLogic.cs
@@ -1,10 +1,10 @@
 ﻿
 namespace GuestBookDemo
 {
-	public static class GuestLogic
-	{
-		public static void WelcomeMessage()
-		{
+    public static class GuestLogic
+    {
+        public static void WelcomeMessage()
+        {
             Console.WriteLine("Welcome to the Guest Book App");
             Console.WriteLine("******************************");
             Console.WriteLine();
@@ -33,6 +33,50 @@ namespace GuestBookDemo
 
             return output;
         }
+
+        public static bool AskToContinue()
+        {
+            Console.Write("Are there more guests coming (yes/no)?: ");
+            string continueLooping = Console.ReadLine();
+
+            bool output = (continueLooping.ToLower() == "yes");
+
+            return output;
+        }
+
+        public static (List<string> guests, int total) GetAllGuests()
+        {
+
+            int totalGuests = 0;
+            List<string> guests = new List<string>();
+
+            do
+            {
+                // GetPartyName
+                guests.Add(GetPartyName());
+
+                // GetPartySize
+                totalGuests += GetPartySize();
+
+            } while (AskToContinue());
+
+            return (guests, totalGuests);
+        }
+
+        public static void DisplayGuests(List<string> guests)
+        {
+            foreach (string guest in guests)
+            {
+                Console.WriteLine(guests);
+            }
+        }
+
+        public static void DisplayGuestCount(int totalGuests)
+        {
+            Console.WriteLine("Thankyou for everyone who attended.");
+            Console.WriteLine($"The total guest count for this evening was {totalGuests}");
+        }
+
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
 ﻿
 
 //1.Welcome the user(Intro the app name as well)
-//Ask user for their name
-//Store the name
-
-
+    //Ask user for their name
+    //Store the name
 //2. Ask user for number in party
-//<convert string to number>
-//Add the number to the total
-
-
+    //<convert string to number>
+    //Add the number to the total
 //3. Check to see if more guests are coming
-//Loop back to the top until no more guests
-
-
+    //Loop back to the top until no more guests
 //4. Loop over the names list and print them
-
-
 //5. Print the total guest count
 
 using GuestBookDemo;
@@ -24,8 +16,8 @@ using GuestBookDemo;
 // WelcomeMessage
 GuestLogic.WelcomeMessage();
 
-// GetPartyName
-string partyName = GuestLogic.GetPartyName();
+var (guests, totalGuests) = GuestLogic.GetAllGuests();
 
-// GetPartySize
-int partySize = GuestLogic.GetPartySize();
+GuestLogic.DisplayGuests(guests);
+
+GuestLogic.DisplayGuestCount(totalGuests);
